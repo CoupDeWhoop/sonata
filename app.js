6bbcb2f509ff1edc5ceb71c62d9578a962832f1e
CoupDeWhoop <greenland.david@gmail.com>
2024-01-29 10:37:12 +0000

    setting up imports

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const { getLessons } = require('./controllers/lessons.controller')
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const { getLessons } = require('./controllers/lessons.controller');
 
 const app = express();
+const corsOptions = {credentials: true, origin: process.env.URL || '*'};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/api/lessons', getLessons);
 
