f8849e3829ca8e1848af5644f5308e94a4faabcb
CoupDeWhoop <greenland.david@gmail.com>
2024-03-05 15:00:49 +0000

    update jwt expiry duration

--- utils/jwt-helpers.js
+++ utils/jwt-helpers.js
@@ -5,7 +5,7 @@ exports.jwtTokens = ({ user_id, user_email }) => {
     const role = user_email === adminEmail ? 'admin' : 'user';
 
     const user = {user_id, user_email, role};
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'3m'});
-    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET,{expiresIn: '5m'});
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1m'});
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET,{expiresIn: '28d'});
     return ({accessToken, refreshToken});
 }
