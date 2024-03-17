844171db03bcffec4d5f0722736a0bd475a984d4
CoupDeWhoop <greenland.david@gmail.com>
2024-03-17 18:23:10 +0000

    Ordered lessons by date desc

--- models/lessons.model.js
+++ models/lessons.model.js
@@ -48,10 +48,11 @@ exports.fetchUserLessonsAndNotes = (user_id, lesson_id) => {
     WHERE lessons.user_id = $1`
 
     if (lesson_id) {
-        queryStr += ' AND lesson_notes.lesson_id = $2;';
+        queryStr += ' AND lesson_notes.lesson_id = $2';
         queryValues.push(lesson_id)
     }
 
+    queryStr += ' ORDER BY lessons.lesson_timestamp DESC;'
     return db.query(queryStr, queryValues)
     .then(({rows}) => {
         if(rows.length === 0) return Promise.reject({status: 404, msg: 'Lesson not found'})
